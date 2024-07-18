a2493330e3b0758b29023139f92137166c797a37
MUEID11 <mehedirangpur3@gmail.com>
2024-07-18 11:28:49 +0600

    route protection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.js
 import {
   FaHome,
   FaUsers,
@@ -7,15 +6,19 @@ import {
   FaHistory,
   FaPlus,
   FaSignOutAlt,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
-
 import logo from "./../public/logo1.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "./Provider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import useProtection from "./Hooks/useProtection";
 import BalanceButton from "./Components/BlanceButton";
-function App() {
+import Loading from "./Components/Loading";
+
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const { logOut } = useContext(AuthContext);
   const { data, isLoading, isError, refetch } = useProtection();
@@ -25,13 +28,26 @@ function App() {
     logOut();
     return navigate("/login");
   };
+
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
-      <aside className="bg-[#1E90FF] text-gray-100 w-64 hidden sm:flex-none sm:flex flex-col justify-between p-4 ">
+      <aside
+        className={`bg-[#1E90FF] text-gray-100 w-64 sm:flex-none sm:flex flex-col justify-between p-4 fixed inset-y-0 left-0 z-30 min-w-64 md:w-72 lg:w-96 shadow-lg sm:min-h-screen transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out sm:translate-x-0 sm:relative sm:w-96 sm:block`}
+      >
         <div>
-          <div className="sm:text-2xl font-bold mb-6 flex items-center">
-            <img className="h-4 sm:h-8 mr-2" src={logo} alt="logo" /> Len Den
+          <div className="sm:text-2xl font-bold mb-6 flex items-center justify-between">
+            <div className="flex items-center">
+              <img className="h-4 sm:h-8 mr-2" src={logo} alt="logo" /> Len Den
+            </div>
+            <button
+              onClick={() => setIsOpen(false)}
+              className="text-gray-100 hover:text-gray-200 focus:outline-none sm:hidden"
+            >
+              <FaTimes />
+            </button>
           </div>
           <nav className="space-y-4">
             <a
@@ -89,8 +105,21 @@ function App() {
       {/* Main content */}
       <div className="flex-grow bg-blue-100 text-gray-100">
         <header className="flex items-center justify-between mb-6 bg-[#1e9aff] p-4 shadow-xl">
-          <h1 className="sm:text-xl font-medium sm:font-bold ">{`${data.name} (${data.role})`}</h1>
-          <BalanceButton />
+          <div className="flex items-center">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="sm:hidden text-gray-200 hover:text-gray-400 focus:outline-none"
+            >
+              {isOpen ? <FaTimes /> : <FaBars />}
+            </button>
+            <h1 className="sm:text-xl font-medium sm:font-bold ml-4">
+              {data ? `${data.name} (${data.role})` : <Loading />}
+            </h1>
+          </div>
+         
+          <div>
+            <BalanceButton />
+          </div>
         </header>
 
         <div className="grid grid-cols-3 gap-4 text-gray-800">
@@ -101,6 +130,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,10 +1,11 @@
-
 const Loading = () => {
-    return (
-        <div>
-            Loading.....
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center space-x-2">
+      <div className="w-4 h-4 rounded-full animate-pulse dark:bg-blue-800"></div>
+      <div className="w-4 h-4 rounded-full animate-pulse dark:bg-blue-800"></div>
+      <div className="w-4 h-4 rounded-full animate-pulse dark:bg-blue-800"></div>
+    </div>
+  );
 };
 
 export default Loading;
